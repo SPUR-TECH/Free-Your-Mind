7ff6b4a1355b7ad96076b77c4240b5d7c6635c7e
Scott Atkinson <scott.atkin79@gmail.com>
2021-12-20 15:50:14 +0000

    add parallax effectv with js

---
+++ index.js
@@ -0,0 +1,10 @@
+function parallax(element, distance, speed) {
+    const items = document.querySelector(element);
+    items.style.transform = `translateY(${distance * speed}px)`;
+}
+
+window.addEventListener("scroll", function () {
+    parallax(".first-title", window.scrollY, 0.5);
+    parallax(".second-title", window.scrollY, 0.5);
+    parallax(".third-title", window.scrollY, 0.5);
+});
