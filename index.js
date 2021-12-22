837275562ff4eb7c5f76dab139cea372fd60e19e
Scott Atkinson <scott.atkin79@gmail.com>
2021-12-22 13:34:28 +0000

    change med-pose image to inhance performance rating

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function parallax(element, distance, speed) {
 }
 
 window.addEventListener("scroll", function () {
-    parallax(".first-title", window.scrollY, 0.5);
+    parallax(".first-title", window.scrollY, 0.4);
     parallax(".second-title", window.scrollY, 0.48);
     parallax(".third-title", window.scrollY, 0.5);
-    parallax(".forth-title", window.scrollY, 0.48);
+    parallax(".forth-title", window.scrollY, 0.46);
 });
