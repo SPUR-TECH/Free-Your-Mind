7db02b7e214df28cdfcd8053b696df4179125f27
Scott Atkinson <scott.atkin79@gmail.com>
2022-01-09 21:30:20 +0000

    continue with js on sign up page

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,51 +1,81 @@
 // sign up form validation //
 
-const form = document.getElementById("form");
-const firstName = document.getElementById("first-name");
-const lastName = document.getElementById("last-name");
-const email = document.getElementById("email");
-const username = document.getElementById("username");
-const password = document.getElementById("password");
-const password2 = document.getElementById("password2");
+const form = document.getElementById('form');
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById('email');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', e => {
     e.preventDefault();
 
     checkInputs();
 });
 
-// get values from inputs 
-
 function checkInputs() {
-    const firstNameValue = "first-name".value;
-    const lastNameValue = "last-name".value;
-    const emailValue = "email".value;
-    const usernameValue = "username".value;
-    const passwordValue = "password".value;
-    const password2Value = "password2".value;
 
+    const firstNameValue = firstName.value;
+    const lastNameValue = lastName.value;
+    const emailValue = email.value;
+    const usernameValue = username.value;
+    const passwordValue = password.value;
+    const password2Value = password2.value;
 
-    if (firstNameValue === "") {
-
-        // show error icon
-
-        setErrorFor("first-name", "First name can not be blank");
+    if (firstNameValue === '') {
+        setErrorFor(firstName, 'First name cannot be blank');
     } else {
+        setSuccessFor(username);
+    }
 
-        // show success icon
+    if (lastNameValue === '') {
+        setErrorFor(lastName, 'Last name cannot be blank');
+    } else {
+        setSuccessFor(username);
+    }
 
-        setSuccessFor("first-name")
+    if (usernameValue === '') {
+        setErrorFor(username, 'Username cannot be blank');
+    } else {
+        setSuccessFor(username);
+    }
 
+    if (emailValue === '') {
+        setErrorFor(email, 'Email cannot be blank');
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Not a valid email');
+    } else {
+        setSuccessFor(email);
+    }
+
+    if (passwordValue === '') {
+        setErrorFor(password, 'Password cannot be blank');
+    } else {
+        setSuccessFor(password);
+    }
+
+    if (password2Value === '') {
+        setErrorFor(password2, 'Password2 cannot be blank');
+    } else if (passwordValue !== password2Value) {
+        setErrorFor(password2, 'Passwords does not match');
+    } else {
+        setSuccessFor(password2);
     }
 }
 
 function setErrorFor(input, message) {
     const userDetails = input.parentElement;
-    const small = userDetails.querySelector("small");
-
-    // show Error Message
-
+    const small = userDetails.querySelector('small');
+    userDetails.className = '.user-details error';
     small.innerText = message;
+}
 
-    userDetails.className = "userDetails error";
+function setSuccessFor(input) {
+    const userDetails = input.parentElement;
+    userDetails.className = '.user-details success';
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
